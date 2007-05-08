72ff460285f2117d794b4d205a62381c4a0a5563
gh0st <gh0st@ffeb7738-a551-0410-a847-4b7fbc4d3c0e>
2007-05-08 10:18:44 +0000

    Added c00l dns plugin enhancement.

--- plugins/dns_plugin.py
+++ plugins/dns_plugin.py
@@ -1,15 +1,61 @@
 #$ neutron_plugin 01
 
 import socket
+import string
 
+# Begin Gh0st addition
+def get_extended_info(name):
+    server = name.strip()
+    if len(server) == 0:
+	    server = 'www.sex.com'
+    port = 80
+    has_ip = 1
+    try:
+	ipaddr = socket.gethostbyname(server) 
+    except:
+	has_ip = 0
+    reply = ''
+    server_string = ''
+    s = ''
+    if has_ip == 1:    
+	conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+	    conn.connect((socket.gethostbyname(server),port))
+	    conn.send('HEAD / HTTP/1.0 \r\n\r\n')
+	    s = conn.recv(256)
+	    conn.close()
+	except socket.error:
+	    pass	
+
+	if len(s) !=0 :
+	    for line in string.split(s, "\r\n"):
+        	if string.find(line,'Server:') == 0:
+            	    server_string = line
+		    server_string = string.replace(server_string, 'Server:', '')
+
+	reply = 'IP Address: ' + ipaddr + '\r\n'
+
+	if len(server_string) !=0:
+
+	    reply = reply +  'Server Type: ' + server_string + '\r\n'
+	    server_web = 'active'	
+
+	else:
+	    server_web = 'offline'
+	reply = reply +  'Website Status: ' + server_web 	
+
+    else:
+	reply = 'Unable to resolve'
+    return reply
+
+# End Gh0st addition
 def dns_query(query):
 	try:
 		int(query[-1])
 	except ValueError:
-		try:
-			return socket.gethostbyname(query)
-		except socket.gaierror:
-			return 'Unable to Resolve'
+		# Patched by Gh0st
+		return get_extended_info(query)
+		
 	else:
 		try:
 			(hostname, aliaslist, ipaddrlist) = socket.gethostbyaddr(query)
