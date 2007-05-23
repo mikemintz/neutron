7042ea753fb9872099187c8ff958940a5a06a78c
gh0st <gh0st@ffeb7738-a551-0410-a847-4b7fbc4d3c0e>
2007-05-23 09:30:14 +0000

    Added PyAIML Support. Look into code, if you want this.

--- neutron.py
+++ neutron.py
@@ -460,6 +460,22 @@ def dcCB():
 	else:
 		sys.exit(0)
 
+def load_aiml():
+    global k
+    # The Kernel object is the public interface to
+    # the AIML interpreter.
+    k = aiml.Kernel()
+
+    # Use the 'learn' method to load the contents
+    # of an AIML file into the Kernel.
+    k.learn("std-startup.xml")
+
+    # Use the 'respond' method to compute the response
+    # to a user's input string.  respond() returns
+    # the interpreter's response, which in this case
+    # we ignore.
+    k.respond("load aiml b")
+
 ################################################################################
 
 def start():
@@ -468,6 +484,8 @@ def start():
 
 	get_access_levels()
 	load_plugins()
+	# Uncomment if you want PyAIML support.
+	# load_aiml()
 	load_initscript()
 
 	if JCON.connect():
