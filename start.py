c9501cb4c4f8552217af84e0e5741e10b193dde3
gh0st <gh0st@ffeb7738-a551-0410-a847-4b7fbc4d3c0e>
2007-08-08 09:11:18 +0000

    Be careful with this file :)

--- start.py
+++ start.py
@@ -4,11 +4,36 @@
 # It will restart neutron over and over again.
 # And plz, do not forget to set AUTO_RESTART to 0 in config.txt file.
 
+import os
 from os import system
+import time
 from sys import exit as sys_exit
 
+def pyc_cleanup():
+    lines_count = 0
+    print 'Removing python byte-compiled files...'
+    pipe = os.popen("sh -c 'find modules/ -name \'*.pyc\''")
+    lines = pipe.read()
+    s = str(0)
+    if lines.strip():
+	for line in lines.split('\n'):
+	    if line.strip():
+		print line
+		os.remove(line)
+		lines_count += 1
+	s = str(lines_count)	
+    print 'Done. %s file(s) removed'%s
+    
 while True:
     try:
+	# some strange bugs, when .pyc files remain...
+	pyc_cleanup()
+	# do launch neutron
         system("python ./neutron.py")
+	# give time for User Interrupt.
+	# and avoid high CPU load
+	# if something is wrong with
+	# neutron.py
+	time.sleep(10)
     except KeyboardInterrupt:
         sys_exit()
