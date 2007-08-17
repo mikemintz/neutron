35853e1f4e603a39d14975d3b4d7cb3a871077e5
gh0st <gh0st@ffeb7738-a551-0410-a847-4b7fbc4d3c0e>
2007-08-17 15:00:42 +0000

    Added !calc functionality.

--- plugins/python_plugin.py
+++ plugins/python_plugin.py
@@ -1,19 +1,35 @@
 #$ neutron_plugin 01
 
+import re
+import string
+
 def handler_python_eval(type, source, parameters):
 	try:
 		return_value = str(eval(parameters))
-		time.sleep(3)
+		time.sleep(1)
 	except:
 		return_value = str(sys.exc_info()[0]) + ' - ' + str(sys.exc_info()[1])
 	smsg(type, source, return_value)
+
+def handler_python_calc(type, source, parameters):
+	parameters = parameters.strip()
+	if re.sub('([' + string.digits +']|[\+\-\/\*\^\.])','',parameters).strip() == '':
+	    try:
+    		return_value = str(eval(parameters))
+		time.sleep(1)
+	    except:
+		return_value = str(sys.exc_info()[0]) + ' - ' + str(sys.exc_info()[1])
+	else:
+		return_value = 'Invalid syntax.'
+	smsg(type, source, return_value)
+
 
 def handler_python_exec(type, source, parameters):
 	if '\n' in parameters and parameters[-1] != '\n':
 		parameters += '\n'
 	try:
 		exec parameters in globals()
-		time.sleep(3)
+		time.sleep(1)
 		return_value = 'Successful Execution'
 	except:
 		return_value = str(sys.exc_info()[0]) + ' - ' + str(sys.exc_info()[1])
@@ -27,6 +43,7 @@ def handler_python_sh(type, source, parameters):
 	return_value = pipe.read(4096)
 	smsg(type, source, unicode(return_value, 'utf8'))
 
+register_command_handler(handler_python_calc, '!calc', 0, 'Evaluates and returns a Python arithmetic expression.', '!calc <expression>', ['!calc 1+1'])
 register_command_handler(handler_python_eval, '!eval', 100, 'Evaluates and returns a Python expression.', '!eval <expression>', ['!eval 1+1'])
 register_command_handler(handler_python_exec, '!exec', 100, 'Runs a Python statement.', '!exec <statement>', ['!eval pass'])
 register_command_handler(handler_python_sh, '!sh', 100, 'Executes a shell command.', '!sh <command>', ['!sh ls'])
