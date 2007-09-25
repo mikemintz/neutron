7d9320bf1f7680efaef5b7d4ad0a41c13c6aad61
gh0st <gh0st@ffeb7738-a551-0410-a847-4b7fbc4d3c0e>
2007-09-25 15:44:35 +0000

    yep, now it works under m$win :)

--- start.py
+++ start.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python
 
+# Author: Bohdan Turkynewych, AKA Gh0st, 2006-2007, tb0hdan[at]gmail[dot]com
+# Distributed under the terms of GPLv2
+
 # Run this script under screen (man screen)
 # It will restart neutron over and over again.
 # And plz, do not forget to set AUTO_RESTART to 0 in config.txt file.
 
-import os
-from os import system
-import time
+from time import sleep as time_sleep
 from sys import exit as sys_exit
+from os import walk as os_walk, remove as os_remove, system as os_system
+from re import sub as re_sub
+from os.path import join as os_path_join, getsize as os_path_getsize
 
 def pyc_cleanup():
     lines_count = 0
     print 'Removing python byte-compiled files...'
-    pipe = os.popen("sh -c 'find modules/ -name \'*.pyc\''")
-    lines = pipe.read()
-    s = str(0)
-    if lines.strip():
-	for line in lines.split('\n'):
-	    if line.strip():
-		print line
+    for root, dirs, files in os_walk('modules'):
+	for name in files:
+	    fpath =  os_path_join(root, name)
+	    fsize =  os_path_getsize(fpath)
+	    if re_sub('(^.*\.pyc$)','',fpath) == '':
+		print 'File: ' + str(fpath) + ' Bytes: ' + str(fsize)
 		try:
-		    os.remove(line)
+		    os_remove(fpath)
 		except:
 		    print 'Check permissions and/or FS.'
 		    raise
 		lines_count += 1
-	s = str(lines_count)	
-    print 'Done. %s file(s) removed'%s
+    print 'Done. %s file(s) removed'%str(lines_count)
     
 while True:
     try:
 	# some strange bugs, when .pyc files remain...
 	pyc_cleanup()
 	# do launch neutron
-        system("python ./neutron.py")
+        os_system("python ./neutron.py")
 	# give time for User Interrupt.
 	# and avoid high CPU load
 	# if something is wrong with
 	# neutron.py
-	time.sleep(10)
+	time_sleep(10)
     except KeyboardInterrupt:
         sys_exit()
