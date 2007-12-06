6468743f82b5c3fe6e2d4ca0649a2f2060f0ff06
gh0st <gh0st@ffeb7738-a551-0410-a847-4b7fbc4d3c0e>
2007-12-06 19:13:17 +0000

    Added maxmind.com query capability (25 req/ip/day, consider using tor... )

--- plugins/dns_plugin.py
+++ plugins/dns_plugin.py
@@ -48,11 +48,50 @@ def get_extended_info(name):
 
 	else:
 	    server_web = 'offline'
-	reply = reply +  'Website Status: ' + server_web 	
+	reply = reply +  'Website Status: ' + server_web
+	reply += '\r\n' + geoip_get(ipaddr)
 
     else:
 	reply = 'Unable to resolve'
     return reply
+
+def geoip_get(parameters):
+    if parameters.strip()=='':
+	smsg(type, source, 'Empty Input')
+	return
+    else:
+	parameters = parameters.strip()
+	if len(parameters)>15:
+			smsg(type, source, 'Wrong format')
+			return
+	postData = urllib.urlencode({'ips':parameters,'type':'','u':'','p':''})
+        req2 = urllib2.Request('http://www.maxmind.com/app/locate_ip',postData)
+	req2.add_header = ('User-agent', 'Mozilla/5.0')
+        try:
+    	    r = urllib2.urlopen(req2)
+    	    target = r.read()
+    	    od = re.search('Edition Results</span><p>',target)
+    	    message = target[od.end():]
+    	    message = message[:re.search('</table>',message).start()]
+	    if len(message) != 0:
+    		message = '\n' + message.strip()
+	    else:
+		message = 'Ooops. Nothing ;-)'
+	    reply = ''
+	    mass=[]
+	    for line in message.split('\n'):
+    		if re.match(' ',line):
+		    line = line.replace(' ','')
+		if re.search('<(td|th)>',line) and not re.search('Hostname',line):
+	    	    mass.append(decode(line))
+	    for i in xrange(0,12):
+		if mass[i+13].strip() != '':
+		    reply += str(mass[i]) + ': ' + str(mass[i+13]) + '\r\n'
+	    maplink = 'http://maps.google.com/maps?q=' + str(mass[19]) + ',+' + str(mass[20])+ '&iwloc=A&hl=en'
+	    reply += 'Google Maps URL: ' + maplink
+	    return reply
+        except:
+	    return 'Error occured while querying maxmind.com'
 
 # End Gh0st addition
 def dns_query(query):
